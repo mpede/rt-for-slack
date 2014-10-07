b5cbc4c96ebfbcee9b0f44b7268369ad5aa31d8f
Christian Schmidt <cschmidt@acceleration.biz>
2014-10-07 23:33:51 +0200

    first hotfix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var http = require('http');
+var request = require('request');
 var inspect = require('eyes').inspector({  maxLength:null, stream:null });
 
 http.createServer(function (req, res) {
@@ -13,10 +14,15 @@ http.createServer(function (req, res) {
         incoming+=d;
     })
     req.on('end',function(){
+        incoming=QueryStringToJSON(incoming);
         res.writeHead(200, {'Content-Type': 'text/plain'});    
-        res.write('\nBOM\n'+incoming+'\n');
-        res.write(inspect(QueryStringToJSON(incoming)).replace(/\[[\d]{1,2}m/g,''));
-        res.end('\nEOM\n(played back to you by NodeJS ;-)');
+        //res.write('\nBOM\n'+incoming+'\n');
+        res.write(inspect(incoming).replace(/\[[\d]{1,2}m/g,''));
+        res.write('\n');
+        request.get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?q='+incoming.text+'&page_limit=4&page=1&apikey=***REMOVED***',function(e,r){
+                res.write(inspect(JSON.parse(r.body)).replace(/\[[\d]{1,2}m/g,''));
+                res.end('\n\n(played back to you by NodeJS ;-)');
+        });
     })
 
 }).listen(port, ipaddress);
