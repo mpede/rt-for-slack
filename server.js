06d0c0103e1e2386a8dba86cfc00525b3b9962c5
Christian Schmidt <cschmidt@acceleration.biz>
2014-10-07 23:03:47 +0200

    first hotfix

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ http.createServer(function (req, res) {
     })
     req.on('end',function(){
         res.writeHead(200, {'Content-Type': 'text/plain'});    
-        res.write('\nSOM\n');
+        res.write('\nBOM\n');
         res.write(inspect(incoming).replace(/\[[\d]{1,2}m/g,''));
-        res.end('\nEOM\n');
+        res.end('\nEOM\n(played back to you by NodeJS ;-)');
     })
 
 }).listen(port, ipaddress);
