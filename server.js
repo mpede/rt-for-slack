b045629fdc264c5f7da2e415f793e4224e383cd9
Christian Schmidt <cschmidt@acceleration.biz>
2014-10-10 22:15:00 +0200

    restart

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var
     QueryStringToJSON= require('./tools').QueryStringToJSON;
     ip = process.env.OPENSHIFT_NODEJS_IP||'127.0.0.1',
     port = process.env.OPENSHIFT_NODEJS_PORT||8080,
-    data={};
+    data={};  
 
 http.createServer(function (req, res) { 
     data.incoming="";
